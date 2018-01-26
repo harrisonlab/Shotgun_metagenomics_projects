bdf53dca4b5210306e3f60335ee6938b499f7964
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-01-26 09:23:36 +0000

    Update preprocessing.sh

--- ARD/preprocessing.sh
+++ ARD/preprocessing.sh
@@ -20,3 +20,33 @@ for FR in $PROJECT_FOLDER/data/fastq/*_1.fq.gz; do
   $PROJECT_FOLDER/metagenomics_pipeline/common/resources/adapters/truseq.fa \
   4
 done
+
+# phix filter
+for FR in $PROJECT_FOLDER/data/trimmed/*_1.fq.gz.trimmed.fq.gz; do
+  RR=$(sed 's/_1/_2/' <<< $FR)
+  $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c filter -p bbduk \
+  $PROJECT_FOLDER/metagenomics_pipeline/common/resources/contaminants/phix_174.fa \
+  $PROJECT_FOLDER/data/filtered \
+  $FR \
+  $RR  \
+  k=31 \
+  hdist=1
+done
+
+# human contaminant filter
+for FR in $PROJECT_FOLDER/data/filtered/*_1.fq.gz.trimmed.fq.gz.filtered.fq.gz; do
+  RR=$(sed 's/_1/_2/' <<< $FR)
+  $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c filter -p bbmap \
+  $PROJECT_FOLDER/metagenomics_pipeline/common/resources/contaminants/bbmap_human \
+  $PROJECT_FOLDER/data/cleaned \
+  $FR \
+  $RR \
+  minid=0.95 \
+  maxindel=3 \
+  bwr=0.16 \
+  bw=12 \
+  quickmatch \
+  fast \
+  minhits=2 \
+  t=8
+done
