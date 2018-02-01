eb4f41dafd3346ff77867f40f8177ea6f86a59a4
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-02-01 12:07:43 +0000

    Create assembly.sh

---
+++ ARD/assembly.sh
@@ -0,0 +1,35 @@
+# metaspades
+for FR in $PROJECT_FOLDER/data/corrected/*_1.fq.gz.trimmed.fq.gz.filtered.fq.gz.cleaned.fq.gz.corrected.fq.gz; do
+  RR=$(sed 's/_1/_2/' <<< $FR)
+  PREFIX=$(grep -Po 'N[0-9]+.' <<<$FR)
+  $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p metaspades \
+  $PROJECT_FOLDER/data/assembled \
+  $FR \
+  $RR  \
+  $PREFIX \
+  -k 21,33,55,77
+done
+
+# megahit (using pre-merged reads)
+for FR in $PROJECT_FOLDER/data/merged/*_1.unmerged.fq.gz; do
+  RR=$(sed 's/_1/_2/' <<< $FR)
+  MR=$(sed 's/_1\.un/\./' <<< $FR)
+  PREFIX=$(grep -Po 'N[0-9]+.' <<<$FR)
+  $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit \
+  $PROJECT_FOLDER/data/assembled/megahit_merged \
+  $PREFIX \
+  -r $MR,$FR,$RR \
+  --k-min=27 --k-step 10 --k-max 127
+done
+
+# megahit (using unmerged reads)
+for FR in $PROJECT_FOLDER/data/corrected/*_1.fq.gz.trimmed.fq.gz.filtered.fq.gz.cleaned.fq.gz.corrected.fq.gz; do
+  RR=$(sed 's/_1/_2/' <<< $FR)
+  MR=$(sed 's/_1\.un/\./' <<< $FR)
+  PREFIX=$(grep -Po 'N[0-9]+.' <<<$FR)
+  $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit \
+  $PROJECT_FOLDER/data/assembled/megahit_unmerged \
+  $PREFIX \
+ -1 $FR -2 $RR -r $MR \
+ --k-min=27 --k-step 10 --k-max 127
+done
