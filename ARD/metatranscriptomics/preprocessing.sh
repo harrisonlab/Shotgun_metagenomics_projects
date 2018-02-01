341612e6725f494c7c368e7361c0c44432d9a54f
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-02-01 16:45:55 +0000

    Create preprocessing.sh

---
+++ ARD/metatranscriptomics/preprocessing.sh
@@ -0,0 +1,55 @@
+# add project folders
+PROJECT_FOLDER=~/projects/myproject
+ln -s ~/pipelines/metatranscriptomics $PROJECT_FOLDER/metatranscriptomics_pipeline
+
+mkdir $PROJECT_FOLDER/data
+mkdir $PROJECT_FOLDER/data/cluster
+mkdir $PROJECT_FOLDER/data/fastq
+mkdir $PROJECT_FOLDER/data/trimmed
+mkdir $PROJECT_FOLDER/data/filtered
+mkdir $PROJECT_FOLDER/data/normalised
+mkdir $PROJECT_FOLDER/data/cleaned
+mkdir $PROJECT_FOLDER/data/corrected
+mkdir $PROJECT_FOLDER/data/merged
+
+#### Adapter trimming
+for FR in $PROJECT_FOLDER/data/fastq/*_1.fq.gz; do
+  RR=$(sed 's/_1/_2/' <<< $FR)
+  $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c trim \
+  $FR \
+  $RR \
+  $PROJECT_FOLDER/data/trimmed \
+  $PROJECT_FOLDER/metagenomics_pipeline/common/resources/adapters/truseq.fa \
+  4
+done
+
+#### Synthetic construct/contaminant filtering 
+for FR in $PROJECT_FOLDER/data/trimmed/*_1.fq.gz.trimmed.fq.gz; do
+  RR=$(sed 's/_1/_2/' <<< $FR)
+  $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c filter -p bbduk \
+  $PROJECT_FOLDER/metagenomics_pipeline/common/resources/contaminants/phix_174.fa \
+  $PROJECT_FOLDER/data/filtered \
+  $FR \
+  $RR \
+  k=31 \
+  hdist=1
+done
+
+
+#### Human contaminant removal (BBMap)
+for FR in $PROJECT_FOLDER/data/filtered/*_1.fq.gz.trimmed.fq.gz.filtered.fq.gz; do
+  RR=$(sed 's/_1/_2/' <<< $FR)
+  $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c filter -p bbmap \
+  $PROJECT_FOLDER/metagenomics_pipeline/common/resources/contaminants/bbmap_human \
+  $PROJECT_FOLDER/data/cleaned \
+  $FR \
+  $RR \
+  minid=0.95 \
+  maxindel=3 \
+  bwr=0.16 \
+  bw=12 \
+  quickmatch \
+  fast \
+  minhits=2 \
+  t=8
+done
