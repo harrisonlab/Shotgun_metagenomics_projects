472906d6f5476986db59949da2f9c11a56f14304
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-05-21 09:37:58 +0100

    Update assembly.sh

--- ARD/metatranscriptomics/assembly.sh
+++ ARD/metatranscriptomics/assembly.sh
@@ -34,9 +34,40 @@ for FR in $PROJECT_FOLDER/data/corrected/*_1.corrected.fq.gz; do
  --k-min=27 --k-step 10 --k-max 127 --bubble-level 0
 done
 
-# align with bbduk 
+# align with bbmap 
+# assemblies are compressed, bbmap requires them to be uncompressed for alignment
+cd $PROJECT_FOLDER/data/assembly
+Find . -maxdepth=3 type=f -name *.gz|xargs -I% pigz -dp 4 %
 
-# megahit spades unaligned
+# align reads to assembly - spades
+for FR in $PROJECT_FOLDER/data/cleaned/*_1.cleaned.fq.gz; do
+  RR=$(sed 's/_1/_2/' <<< $FR)
+  PREFIX=$(grep -Po 'M[0-9]+.' <<<$FR)
+  $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c filter -p bbmap \
+  $PROJECT_FOLDER/data/assembled/spades/$PREFIX/scaffolds.fasta \
+  $PROJECT_FOLDER/data/assembly_checks/spades \
+  $FR \
+  $RR \
+  nodisk=t \
+  kfilter=22 \
+  subfilter=15 \
+  maxindel=80 \
+  unpigz=t \
+  touppercase=t \
+  t=8
+done
+
+# align unaligned spades with megahit
+for FR in $PROJECT_FOLDER/data/assembly_checks/spades/*_1.cleaned.fq.gz.cleaned.fq.gz; do
+  RR=$(sed 's/_1/_2/' <<< $FR)
+  MR=$(sed 's/_1\.un/\./' <<< $FR)
+  PREFIX=$(grep -Po 'N[0-9]+.' <<<$FR)
+  $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit \
+  $PROJECT_FOLDER/data/assembled/megahit_of_spades \
+  $PREFIX \
+ -1 $FR -2 $RR -r $MR \
+ --k-min=27 --k-step 10 --k-max 127
+done
 
 # assemble assemblies
 megahit -o output -t 20 --kmin-1pass --out-prefix all_spades --k-min=27 --k-step 20 --k-max 127 -r \
@@ -54,3 +85,14 @@ $PROJECT_FOLDER/data/assembled/megahit_of_spades/M4H/M4H.contigs.fa.gz,\
 $PROJECT_FOLDER/data/assembled/megahit_of_spades/M11S/M11S.contigs.fa.gz,\
 $PROJECT_FOLDER/data/assembled/megahit_of_spades/M14S/M14S.contigs.fa.gz,\
 $PROJECT_FOLDER/data/assembled/megahit_of_spades/M14H/M14H.contigs.fa.gz &
+
+# assemble assemblies - useful samples
+megahit -o output -t 20 --kmin-1pass --out-prefix all_spades --k-min=27 --k-step 20 --k-max 127 -r \
+$PROJECT_FOLDER/data/assembled/spades/M1H/scaffolds.fasta,\
+$PROJECT_FOLDER/data/assembled/spades/M1S/scaffolds.fasta,\
+$PROJECT_FOLDER/data/assembled/spades/M2H/scaffolds.fasta,\
+$PROJECT_FOLDER/data/assembled/spades/M2S/scaffolds.fasta,\
+$PROJECT_FOLDER/data/assembled/megahit_of_spades/M1H/M1H.contigs.fa.gz,\
+$PROJECT_FOLDER/data/assembled/megahit_of_spades/M1S/M1S.contigs.fa.gz,\
+$PROJECT_FOLDER/data/assembled/megahit_of_spades/M2H/M2H.contigs.fa.gz,\
+$PROJECT_FOLDER/data/assembled/megahit_of_spades/M2S/M2S.contigs.fa.gz 
