a3dce06fc0f06b5e36e68716c50202433ea4c751
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-05-29 11:12:56 +0100

    Create combined_assembly.sh

---
+++ ARD/combined_assembly.sh
@@ -0,0 +1,41 @@
+megahit -o output \
+-t 12 \
+--kmin-1pass \
+--out-prefix megahit_of_spades \
+--k-min=27 --k-step 20 --k-max 127 \
+-r M1H.contigs.fa,\
+M1H.metaofspades.fa,\
+M1H_RNA_1.corrected.fq.gz,\
+M1H_spades.fasta,\
+M1S.contigs.fa,\
+M1S.metaofspades.fa,\
+M1S_RNA_1.corrected.fq.gz,\
+M1S_spades.fasta,\
+M2H.contigs.fa,\
+M2H.metaofspades.fa,\
+M2H_RNA_1.corrected.fq.gz,\
+M2H_spades.fasta,\
+M2S.contigs.fa,\
+M2S.metaofspades.fa,\
+M2S_RNA_1.corrected.fq.gz,\
+M2S_spades.fasta,\
+N11S_DNA_1.corrected.fq.gz,\
+N11S_DNA_2.corrected.fq.gz,\
+N14H_DNA_1.corrected.fq.gz,\
+N14H_DNA_2.corrected.fq.gz,\
+N14S_DNA_1.corrected.fq.gz,\
+N14S_DNA_2.corrected.fq.gz,\
+N1H_DNA_1.corrected.fq.gz,\
+N1H_DNA_2.corrected.fq.gz,\
+N1S_DNA_1.corrected.fq.gz,\
+N1S_DNA_2.corrected.fq.gz,\
+N2H_DNA_1.corrected.fq.gz,\
+N2H_DNA_2.corrected.fq.gz,\
+N2S_DNA_1.corrected.fq.gz,\
+N2S_DNA_2.corrected.fq.gz,\
+N3H_DNA_1.corrected.fq.gz,\
+N3H_DNA_2.corrected.fq.gz,\
+N4H_DNA_1.corrected.fq.gz,\
+N4H_DNA_2.corrected.fq.gz,\
+N4S_DNA_1.corrected.fq.gz,\
+N4S_DNA_2.corrected.fq.gz
