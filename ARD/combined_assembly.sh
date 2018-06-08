92fea1e27782a5b7a4f47cdd490752e205743876
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-06-08 10:53:46 +0100

    Update combined_assembly.sh

--- ARD/combined_assembly.sh
+++ ARD/combined_assembly.sh
@@ -1,41 +1,18 @@
-megahit -o output \
--t 12 \
---kmin-1pass \
---out-prefix megahit_of_spades \
+# assemble spades MT assemblies + MT and MG corrected reads - everything assembly
+for F in $PROJECT_FOLDER/assembly/mt_mg_data/*.gz; do
+  R=$(echo $R,$F)
+done  
+R=$(sed '1s/^,//' <<<$R)
+qsub -l h=blacklace11 qs.sh -pe smp 24 $PROJECT_FOLDER/assembly/everything spades \
 --k-min=27 --k-step 20 --k-max 127 \
--r M1H.contigs.fa,\
-M1H.metaofspades.fa,\
-M1H_RNA_1.corrected.fq.gz,\
-M1H_spades.fasta,\
-M1S.contigs.fa,\
-M1S.metaofspades.fa,\
-M1S_RNA_1.corrected.fq.gz,\
-M1S_spades.fasta,\
-M2H.contigs.fa,\
-M2H.metaofspades.fa,\
-M2H_RNA_1.corrected.fq.gz,\
-M2H_spades.fasta,\
-M2S.contigs.fa,\
-M2S.metaofspades.fa,\
-M2S_RNA_1.corrected.fq.gz,\
-M2S_spades.fasta,\
-N11S_DNA_1.corrected.fq.gz,\
-N11S_DNA_2.corrected.fq.gz,\
-N14H_DNA_1.corrected.fq.gz,\
-N14H_DNA_2.corrected.fq.gz,\
-N14S_DNA_1.corrected.fq.gz,\
-N14S_DNA_2.corrected.fq.gz,\
-N1H_DNA_1.corrected.fq.gz,\
-N1H_DNA_2.corrected.fq.gz,\
-N1S_DNA_1.corrected.fq.gz,\
-N1S_DNA_2.corrected.fq.gz,\
-N2H_DNA_1.corrected.fq.gz,\
-N2H_DNA_2.corrected.fq.gz,\
-N2S_DNA_1.corrected.fq.gz,\
-N2S_DNA_2.corrected.fq.gz,\
-N3H_DNA_1.corrected.fq.gz,\
-N3H_DNA_2.corrected.fq.gz,\
-N4H_DNA_1.corrected.fq.gz,\
-N4H_DNA_2.corrected.fq.gz,\
-N4S_DNA_1.corrected.fq.gz,\
-N4S_DNA_2.corrected.fq.gz
+-r $R
+
+# align MG + MT cleaned reads to everything assembly
+
+for F in $PROJECT_FOLDER/assembly/everything/checks/*.gz; do
+  U=$(echo $U,$F)
+done  
+U=$(sed '1s/^,//' <<<$U)
+qsub -l h=blacklace11  -pe smp 24  qs.sh $PROJECT_FOLDER/assembly/everything/unaligned mega_un \
+--k-min=27 --k-step 20 --k-max 127 \
+-r $U
