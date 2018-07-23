cc9de722c509a24f7bd2d25534410d442d4ef782
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-07-23 10:09:24 +0100

    Update assembly.sh

--- Oak_decline/assembly.sh
+++ Oak_decline/assembly.sh
@@ -1,1 +1,13 @@
-.
+# To speed up assembly cutting data into site specific
+
+# megahit 
+for FR in $PROJECT_FOLDER/data/corrected/*_1.fq.gz; do
+  RR=$(sed 's/_1/_2/' <<< $FR)
+  MR=$(sed 's/_1\.un/\./' <<< $FR)
+  PREFIX=$(grep -Po 'N[0-9]+.' <<<$FR)
+  $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
+  $PROJECT_FOLDER/data/assembled/megahit \
+  $PREFIX \
+ -1 $FR -2 $RR -r $MR \
+ --k-min=27 --k-step 10 --k-max 127
+done
