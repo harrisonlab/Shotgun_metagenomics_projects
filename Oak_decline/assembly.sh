68e28d644ed621bdc0c1e09f50ed8959c048caf5
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-07-23 11:14:15 +0100

    Update assembly.sh

--- Oak_decline/assembly.sh
+++ Oak_decline/assembly.sh
@@ -1,13 +1,64 @@
-# To speed up assembly cutting data into site specific
+# To speed up assembly cutting data into site specific and assembling with megahit (accepts multiple fq input)
 
-# megahit 
-for FR in $PROJECT_FOLDER/data/corrected/*_1.fq.gz; do
-  RR=$(sed 's/_1/_2/' <<< $FR)
-  MR=$(sed 's/_1\.un/\./' <<< $FR)
-  PREFIX=$(grep -Po 'N[0-9]+.' <<<$FR)
-  $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
-  $PROJECT_FOLDER/data/assembled/megahit \
-  $PREFIX \
- -1 $FR -2 $RR -r $MR \
- --k-min=27 --k-step 10 --k-max 127
-done
+# Bigwood
+f=$(ls -m $PROJECT_FOLDER/data/corrected/B*_1.fq.gz|tr -d ' '|tr -d '\n')
+r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
+$PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
+1 blacklace[01][06789].blacklace \
+$PROJECT_FOLDER/data/assembled/megahit \
+BIGWOOD \
+-24 $f -2 $r --k-min=27 --k-step 10 --k-max 127
+
+# Attingham
+f=$(ls -m $PROJECT_FOLDER/data/corrected/A*_1.fq.gz|tr -d ' '|tr -d '\n')
+r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
+$PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
+1 blacklace[01][06789].blacklace \
+$PROJECT_FOLDER/data/assembled/megahit \
+ATTINGHAM \
+-24 $f -2 $r --k-min=27 --k-step 10 --k-max 127
+
+# Chestnuts
+f=$(ls -m $PROJECT_FOLDER/data/corrected/C*_1.fq.gz|tr -d ' '|tr -d '\n')
+r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
+$PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
+1 blacklace[01][06789].blacklace \
+$PROJECT_FOLDER/data/assembled/megahit \
+CHESTNUTS \
+-24 $f -2 $r --k-min=27 --k-step 10 --k-max 127
+
+# Gt_Monk
+f=$(ls -m $PROJECT_FOLDER/data/corrected/G*_1.fq.gz|tr -d ' '|tr -d '\n')
+r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
+$PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
+1 blacklace[01][06789].blacklace \
+$PROJECT_FOLDER/data/assembled/megahit \
+GT_MONK \
+-24 $f -2 $r --k-min=27 --k-step 10 --k-max 127
+
+# Langdale
+f=$(ls -m $PROJECT_FOLDER/data/corrected/L*_1.fq.gz|tr -d ' '|tr -d '\n')
+r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
+$PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
+1 blacklace[01][06789].blacklace \
+$PROJECT_FOLDER/data/assembled/megahit \
+LANGDALE \
+-24 $f -2 $r --k-min=27 --k-step 10 --k-max 127
+
+# Speculation
+f=$(ls -m $PROJECT_FOLDER/data/corrected/S*_1.fq.gz|tr -d ' '|tr -d '\n')
+r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
+$PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
+1 blacklace[01][06789].blacklace \
+$PROJECT_FOLDER/data/assembled/megahit \
+SPECULATION \
+-24 $f -2 $r --k-min=27 --k-step 10 --k-max 127
+
+# Winding
+f=$(ls -m $PROJECT_FOLDER/data/corrected/W*_1.fq.gz|tr -d ' '|tr -d '\n')
+r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
+$PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
+1 blacklace[01][06789].blacklace \
+$PROJECT_FOLDER/data/assembled/megahit \
+WINDING \
+-24 $f -2 $r --k-min=27 --k-step 10 --k-max 127
