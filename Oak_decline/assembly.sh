6b19527905191b57f9b31d08f28d81393f15c6e4
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-07-31 10:33:56 +0100

    Update assembly.sh

--- Oak_decline/assembly.sh
+++ Oak_decline/assembly.sh
@@ -9,52 +9,52 @@ $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit
 10 blacklace01.blacklace \
 $PROJECT_FOLDER/data/assembled/megahit \
 BIGWOOD \
--1 $f -2 $r --k-min=27 --k-step 10 --k-max 127
+-1 $f -2 $r --k-min=27 --k-step 10 --k-max 77
 
 # Attingham
 f=$(ls -m $PROJECT_FOLDER/data/corrected/A*_1.fq.gz|tr -d ' '|tr -d '\n')
 r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
 $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
-12 blacklace11.blacklace \
+24 blacklace11.blacklace \
 $PROJECT_FOLDER/data/assembled/megahit \
 ATTINGHAM \
--m 0.5 -1 $f -2 $r --k-min=27 --k-step 10 --k-max 127
+-1 $f -2 $r --k-min=27 --k-step 10 --k-max 77
 
 # Chestnuts
 f=$(ls -m $PROJECT_FOLDER/data/corrected/C*_1.fq.gz|tr -d ' '|tr -d '\n')
 r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
 $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
-12 blacklace11.blacklace \
+24 blacklace11.blacklace \
 $PROJECT_FOLDER/data/assembled/megahit \
 CHESTNUTS \
--m 0.5 -1 $f -2 $r --k-min=27 --k-step 10 --k-max 127
+-1 $f -2 $r --k-min=27 --k-step 10 --k-max 77
 
 # Gt_Monk
 f=$(ls -m $PROJECT_FOLDER/data/corrected/G*_1.fq.gz|tr -d ' '|tr -d '\n')
 r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
 $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
-12 blacklace11.blacklace \
+24 blacklace11.blacklace \
 $PROJECT_FOLDER/data/assembled/megahit \
 GT_MONK \
--m 0.5 -1 $f -2 $r --k-min=27 --k-step 10 --k-max 127
+-1 $f -2 $r --k-min=27 --k-step 10 --k-max 77
 
 # Langdale
 f=$(ls -m $PROJECT_FOLDER/data/corrected/L*_1.fq.gz|tr -d ' '|tr -d '\n')
 r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
 $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
-12 blacklace11.blacklace \
+24 blacklace11.blacklace \
 $PROJECT_FOLDER/data/assembled/megahit \
 LANGDALE \
--m 0.5 -1 $f -2 $r --k-min=27 --k-step 10 --k-max 127
+-1 $f -2 $r --k-min=27 --k-step 10 --k-max 77
 
 # Speculation
 f=$(ls -m $PROJECT_FOLDER/data/corrected/S*_1.fq.gz|tr -d ' '|tr -d '\n')
 r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
 $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
-12 blacklace11.blacklace \
+24 blacklace11.blacklace \
 $PROJECT_FOLDER/data/assembled/megahit \
 SPECULATION \
--m 0.5 -1 $f -2 $r --k-min=27 --k-step 10 --k-max 127
+-1 $f -2 $r --k-min=27 --k-step 10 --k-max 77
 
 # Winding
 f=$(ls -m $PROJECT_FOLDER/data/corrected/W*_1.fq.gz|tr -d ' '|tr -d '\n')
@@ -63,4 +63,4 @@ $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit
 10 blacklace01.blacklace \
 $PROJECT_FOLDER/data/assembled/megahit \
 WINDING \
--1 $f -2 $r --k-min=27 --k-step 10 --k-max 127
+-1 $f -2 $r --k-min=27 --k-step 10 --k-max 77
