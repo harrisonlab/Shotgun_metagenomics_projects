ab207a64bffc3c4addf46f5bd1c49c7e8768f429
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-07-31 13:37:38 +0100

    Update assembly.sh

--- Oak_decline/assembly.sh
+++ Oak_decline/assembly.sh
@@ -11,14 +11,14 @@ $PROJECT_FOLDER/data/assembled/megahit \
 BIGWOOD \
 -1 $f -2 $r --k-min=27 --k-step 10 --k-max 77
 
-# Attingham
+# Attingham - produces tmp files too large for tmp directory on nodes
 f=$(ls -m $PROJECT_FOLDER/data/corrected/A*_1.fq.gz|tr -d ' '|tr -d '\n')
 r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
 $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
 24 blacklace11.blacklace \
 $PROJECT_FOLDER/data/assembled/megahit \
 ATTINGHAM \
--1 $f -2 $r --k-min=27 --k-step 10 --k-max 77
+-1 $f -2 $r --k-min=27 --k-step 10 --k-max 77 --tmp-dir /data/scratch/deakig/tmp/ATTINGHAM
 
 # Chestnuts
 f=$(ls -m $PROJECT_FOLDER/data/corrected/C*_1.fq.gz|tr -d ' '|tr -d '\n')
@@ -36,16 +36,16 @@ $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit
 24 blacklace11.blacklace \
 $PROJECT_FOLDER/data/assembled/megahit \
 GT_MONK \
--1 $f -2 $r --k-min=27 --k-step 10 --k-max 77
+-1 $f -2 $r --k-min=27 --k-step 10 --k-max 77 
 
-# Langdale
+# Langdale - produces tmp files too large for tmp directory on nodes
 f=$(ls -m $PROJECT_FOLDER/data/corrected/L*_1.fq.gz|tr -d ' '|tr -d '\n')
 r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
 $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
 24 blacklace11.blacklace \
 $PROJECT_FOLDER/data/assembled/megahit \
 LANGDALE \
--1 $f -2 $r --k-min=27 --k-step 10 --k-max 77
+-1 $f -2 $r --k-min=27 --k-step 10 --k-max 77 --tmp-dir /data/scratch/deakig/tmp/LANGDALE
 
 # Speculation
 f=$(ls -m $PROJECT_FOLDER/data/corrected/S*_1.fq.gz|tr -d ' '|tr -d '\n')
