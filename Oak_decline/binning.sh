8fcc00f91ace20f1df241e830aba30948db360aa
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-08-10 09:12:05 +0100

    Update binning.sh

--- Oak_decline/binning.sh
+++ Oak_decline/binning.sh
@@ -6,17 +6,24 @@
 
 # it has three/four steps
 
-# annotate 
+# annotate uses functionalAnnotaion.py, but splits input file into 20,000 droid chunks for running on cluster (25 concurrent jobs)
 #functionalAnnotation.py -m METADATA_FILE -db DATABASE_FILE -e EVALUE_CUTOFF -n N -p MAX_ACCEPTABLE_OVERLAP
 PREFIX=BIGWOOD
 $PROJECT_FOLDER/metagenomics_pipeline/scripts/fun_bin.sh \
-  4 $PROJECT_FOLDER/data/assembled/megahit/$PREFIX \
+  1 $PROJECT_FOLDER/data/assembled/megahit/$PREFIX \
  $PREFIX.contigs.fa \
  ~/pipelines/common/resources/pfam/Pfam-A.hmm \
  -e 1e-03
+
+# concatenate annotate output
+find -type f -name X.gff|head -n1|xargs -I% head -n1 % >$PREFIX.gff
+find -type f -name X.gff|xargs -I% grep -v "##" % >>$PREFIX.gff
+find -type f -name X.pep|xargs -I% cat % >$PREFIX.pep
+find -type f -name X.hmmout|head -n1|xargs -I% head -n3 % >$PREFIX.hmmout   
+find -type f -name X.hmmout|xargs -I% grep -v "#" % >>$PREFIX.hmmout
+find -type f -name X.hmmout|head -n1|xargs -I% tail -n10 % >>$PREFIX.hmmout
  
- # and etc.
- 
- # mapping is not implemented very well in HirBin, will do this seperately with bbmap + HirBin tools to get BAM in correct format
+# mapping
+# mapping is not implemented very well in HirBin, will do this seperately with bbmap + HirBin tools to get BAM in correct format
  
  
