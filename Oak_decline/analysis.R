1b86dae59cc86a0f58be7b47960f790820b3aa09
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-08-15 10:16:53 +0100

    Update analysis.R

--- Oak_decline/analysis.R
+++ Oak_decline/analysis.R
@@ -1,6 +1,11 @@
 library(data.table)
-library(dtplyr)
+library(tidyverse)
+library(DESeq2)
 
 qq <- lapply(list.files(".","*",full.names=T),function(x) fread(x))
-             
-X <-              
+names<- sub("\\..*","",list.files(".","*",full.names=F,recursive=F))             
+qq <- lapply(seq(1:length(qq)),function(i) {X<-qq[[i]];colnames(X)[2] <- names[i];return(X)})
+
+X <- qq %>% reduce(full_join) # plyr method (returns tibble)
+X <- Reduce(function(...) merge(..., all = TRUE), qq) # data table method (returns data table)
+            
