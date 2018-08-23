25192bced23273e0effc7eac29b737e09949cf01
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-08-23 11:16:25 +0100

    Update analysis.R

--- Oak_decline/analysis.R
+++ Oak_decline/analysis.R
@@ -2,15 +2,18 @@
 #       Load libraries
 #===============================================================================
 library(DESeq2)
+library(BiocParallel)
 library(data.table)
 library(tidyverse)
 library(devtools)
 load_all("~/pipelines/metabarcoding/scripts/myfunctions") # install_github(https://.../)
+register(MulticoreParam(12))
 
 #===============================================================================
 #       Load data
 #===============================================================================
 
+####  counts tables
 # get list of count tables
 qq    <- lapply(list.files(".","*",full.names=T),function(x) {fread(x)})
 
@@ -23,6 +26,13 @@ qq    <- lapply(seq(1:length(qq)),function(i) {X<-qq[[i]];colnames(X)[2] <- name
 # merge count tables into single table
 countData <- Reduce(function(...) {merge(..., all = TRUE)}, qq) # data table method (returns data table)
 #countData    <- qq %>% purr::reduce(full_join,by="V1") # plyr method (returns tibble - but not always...)
+
+### count matrix
+countData <- fread("countData")
+names(countData) <- sub("(_ND.*_L)([0-9]*)(.*)","_\\2",names(countData))
+
+### sub bins
+countData <- fread("countData.subbins")
 
 # set NA values to 0
 countData[is.na] <- 0
@@ -43,20 +53,20 @@ annotation <- fread("~/pipelines/common/resources/pfam/names.txt")
 dds <- DESeqDataSetFromMatrix(dt_to_df(countData), dt_to_df(colData), ~1)
 
 # get number of samples per tree
-sample_numbers <- table(sub("[A-Z]$","",dds$Sample))
+#sample_numbers <- table(sub("[A-Z]$","",dds$Sample))
 
-# collapse (mean) samples
+# collapse (mean) samples - could just use sum, then sizeFactors will correct
 dds <- collapseReplicates2(dds,groupby=dds$Sample,simple=T)
 
 # set the dds sizefactor to the number of samples
 # dds$sizeFactor <- as.vector(sample_numbers/2)
 
 # recreate countData and colData
-countData<- round(counts(dds,normalize=T),0)
-colData <- as.data.frame(colData(dds))
+#countData<- round(counts(dds,normalize=T),0)
+#colData <- as.data.frame(colData(dds))
 
 # new dds object with the corrected data set
-dds <- DESeqDataSetFromMatrix(countData,colData,~1)
+#dds <- DESeqDataSetFromMatrix(countData,colData,~1)
 
 #===============================================================================
 #       Differential analysis
@@ -67,6 +77,9 @@ dds <- DESeqDataSetFromMatrix(countData,colData,~1)
 
 # get size factors
 sizeFactors(dds) <-sizeFactors(estimateSizeFactors(dds))
+
+# filter out low counts (especially for subbins - there will be a lot with low counts)
+# dds <- dds[rowSums(counts(dds, normalize=T))>5,]
 
 # p value for FDR cutoff
 alpha <- 0.1
@@ -81,11 +94,11 @@ dds$Block_pair <- as.factor(dds$Block_pair)
 # add full model to dds object
 design(dds) <- design
 
-# calculate fit
-dds <- DESeq(dds)
+# calculate fit - parallel only useful for subbins
+dds <- DESeq(dds,parallel=T)
 
-# calculate results for default contrast (S vs H)
-res <- results(dds,alpha=alpha)
+# calculate results for default contrast (S vs H) - parallel only useful for subbins
+res <- results(dds,alpha=alpha,parallel=T)
 
 # merge results with annotation
 res_merge <- data.table(inner_join(data.table(NAME=rownames(res),as.data.frame(res)),annotation))
