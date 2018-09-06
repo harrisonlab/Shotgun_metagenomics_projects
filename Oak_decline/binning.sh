ac2ce347dbfe33b3a26e67e7ad44d5a913ba22ba
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-09-06 13:22:49 +0100

    Update binning.sh

--- Oak_decline/binning.sh
+++ Oak_decline/binning.sh
@@ -1,9 +1,9 @@
 # set variables
-$PROJECT_FOLDER=~/projects/OAK_decline/metagenomics
+PROJECT_FOLDER=~/projects/Oak_decline/metagenomics
 PREFIX=BIGWOOD # and etc.
 P1=${PREFIX:0:1}
 
-# identify pfam domains in assembly
+# identify pfam domains in assemblies
 $PROJECT_FOLDER/metagenomics_pipeline/scripts/fun_bin.sh \
  1 $PROJECT_FOLDER/data/assembled/megahit/$PREFIX \
  $PREFIX.contigs.fa \
@@ -19,11 +19,9 @@ find -type f -name X.hmmout|xargs -I% grep -v "#" % >>$PREFIX.hmmout
 find -type f -name X.hmmout|head -n1|xargs -I% tail -n10 % >>$PREFIX.hmmout
 
 grep -v "#" $PREFIX.hmmout|awk -F" " '($21~/^[0-9]+$/) && ($20~/^[0-9]+$/) {print $4,$1,$20,$21,$3,$7}' OFS="\t" > $PREFIX.hmm.cut
-awk -F"\t" '{print $1}' $PREFIX.hmm.cut|sort|uniq > $PREFIX.domains # this is better method as some domains may not be present in gff due to filtering
-# cut -f9 $PREFIX.gff|sort|uniq|sed 's/ID=//'|tail -n +2 > $PREFIX.2.domains # the tail bit gets rid of the first line of output an d the grep removes errors in the output
+Rscript $PROJECT_FOLDER/metagenomics_pipeline/scripts/subbin_domain_extractor.R $PREFIX.hmm.cut $PREFIX.domains
 
 # mapping
-
 bbmap.sh ref=$PREFIX.contigs.fa.gz usemodulo=t #k=11 
 
 for FR in $PROJECT_FOLDER/data/fastq/$P1*_1.fq.gz; do
@@ -52,31 +50,20 @@ for BAM in $PROJECT_FOLDER/data/assembled/aligned/megahit/$P1*.bam; do
   cov
 done
 
+# count bins
 Rscript $PROJECT_FOLDER/metagenomics_pipeline/scripts/cov_count.R "." "$P1.*\\.cov" "$PREFIX.countData"
 
 # Sub binning - convert cov to tab
 for F in $P1*.cov; do
   O=$(sed 's/_.*_L/_L/' <<<$F|sed 's/_1\.cov/.tab/')
-  awk -F"\t" '{sub("ID=","|",$(NF-1));OUT=$1$(NF-1)":"$4":"$5":"$7;print OUT,$NF}' OFS="\t" $F > $O
-done 
-
-for F in $P1*.cov; do
-  O=$(sed 's/_.*_L/_L/' <<<$F|sed 's/_1\.cov/.tab/')
   awk -F"\t" '{sub("ID=","",$(NF-1));OUT=$1"_"$(NF-1)"_"$4"_"$5;print OUT,$(NF-1),$4,$5,$NF}' OFS="\t" $F > $O
 done 
 
+# extract sub bins from proteins file with subbin_fasta_extractor.R - last two args control memory usage (and exection time) 
+Rscript $PROJECT_FOLDER/metagenomics_pipeline/scripts/subbin_fasta_extractor.R $PREFIX.domains $PREFIX.pep "${PREFIX}_clustering/forClustering" 100 T
 
-# then create the required metadata file
-echo -e \
-"Name\tGroup\tReference\tAnnotation\tCounts\tDomain\n"\
-"$PREFIX\tSTATUS\t$PREFIX.pep\t$PREFIX.hmm.cut\tEMPTY\t$PREFIX.domains" > metadata.txt
-
-# The extractSequences module of clusterBinsToSubbins is inpractical for large pep files - the R script subbin_fasta_extractor.R should be used in it's palace
-
-Rscript subbin_fasta_extractor.R $PREFIX.hmm.cut $PREFIX.pep $PREFIX_hirbin_output
-# for some reason this R script didn't work correctly on the latest round. The below awk scripts will quickly fix it, but very odd...
+# for some reason this R script didn't work correctly very odd...
 # it was due to a problem in reordeing the rows - this is now fixed, but below was used for Chestnuts
-
 for F in *.fasta; do
   awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' $F|sed -e '1d' > ${F}.2
 done
@@ -94,9 +81,24 @@ done
 rename 's/\..*/.fasta/' *.3
 
 # Clustering
-clusterBinsToSubbins.py -m $PREFIX.metadata.txt -id 0.7 --reClustering --onlyClustering -f -o  $PREFIX_hirbin_output# clustering without sub bin extraction (no parsing)
-awk -F"\t" '($1~/[HS]/){print $2, $9, $10}' *.uc| \
-awk -F" " '{sub(/_[0-9]+$/,"",$2);sub(/_[0-9]+$/,"",$6);A=$2"_"$3"_"$4"_"$5;if($6~/\*/){B=A}else{B=$6"_"$7"_"$8"_"$9};print A,B}' OFS="\t" > reduced.txt
+$PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c cluster_super_fast \
+  blacklace[01][0-9].blacklace 100 \
+  $PROJECT_FOLDER/data/binning/$PREFIX/${PREFIX}_clustering/forClustering \
+  $PROJECT_FOLDER/data/binning/$PREFIX/${PREFIX}_clustering/clust0.7 \
+  0.7 
+
+awk -F"\t" '($1~/[HS]/){print $2, $9, $10}' $PROJECT_FOLDER/data/binning/${PREFIX}_clustering/clust0.7/*.uc| \
+awk -F" " '{
+  sub(/_[0-9]+$/,"",$2);
+  sub(/_[0-9]+$/,"",$6);
+  A=$2"_"$3"_"$4"_"$5;
+  if($6~/\*/) {
+    B=A
+  } else{
+    B=$6"_"$7"_"$8"_"$9
+  };
+  print A,B
+}' OFS="\t" > $PROJECT_FOLDER/data/binning/$PREFIX/reduced.txt
 
 # parse 
 Rscript subbin_parser.R reduced.txt *.tab $PREFIX.countData.sub_bins
