36f44276586c8096798ecf9a259afd1ac8fef0c3
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-09-13 11:24:45 +0100

    Update binning.sh

--- ARD/binning.sh
+++ ARD/binning.sh
@@ -1,7 +1,7 @@
 # set variables
 PROJECT_FOLDER=~/projects/ARD/metagenomics
-PREFIX="" # and etc.
-P1=${PREFIX:0:1}
+PREFIX=COMBINED # and etc.
+# P1=${PREFIX:0:1}
 
 # identify pfam domains in assemblies
 $PROJECT_FOLDER/metagenomics_pipeline/scripts/fun_bin.sh \
@@ -14,9 +14,6 @@ $PROJECT_FOLDER/metagenomics_pipeline/scripts/fun_bin.sh \
 find -type f -name X.gff|head -n1|xargs -I% head -n1 % >$PROJECT_FOLDER/data/binning/$PREFIX/$PREFIX.gff
 find -type f -name X.gff|xargs -I% grep -v "##" % >>$PROJECT_FOLDER/data/binning/$PREFIX/$PREFIX.gff
 find -type f -name X.pep|xargs -I% cat % >$PROJECT_FOLDER/data/binning/$PREFIX/$PREFIX.pep
-# find -type f -name X.hmmout|head -n1|xargs -I% head -n3 % >$PREFIX.hmmout   
-# find -type f -name X.hmmout|xargs -I% grep -v "#" % >>$PREFIX.hmmout
-# find -type f -name X.hmmout|head -n1|xargs -I% tail -n10 % >>$PREFIX.hmmout
 find -type f -name X.hmmout|xargs -I% grep -v "#" % | \
 awk -F" " '($21~/^[0-9]+$/) && ($20~/^[0-9]+$/) {print $4,$1,$20,$21,$3,$7}' OFS="\t"| \
 $PROJECT_FOLDER/metagenomics_pipeline/scripts/subbin_domain_extractor.pl \
@@ -35,27 +32,28 @@ $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c cluster_super_fast 
 # concatenate clustering output
 cat $PROJECT_FOLDER/data/binning/${PREFIX}_clustering/clust0.7/*.uc > $PROJECT_FOLDER/data/binning/$PREFIX/reduced.txt
 
-# mapping
-bbmap.sh ref=$PREFIX.contigs.fa.gz usemodulo=t #k=11 
+# create bbmap assembly reference
+bbmap.sh ref=FINAL_COMBINED.assembly.fa # usemodulo=t assembly is small no need for this
 
-for FR in $PROJECT_FOLDER/data/fastq/$P1*_1.fq.gz; do
+# map to assembly (final flag limits Java memory to 31G)
+for FR in $PROJECT_FOLDER/data/cleaned/*_1.fq.gz; do
   RR=$(sed 's/_1/_2/' <<< $FR)
   $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c align -p bbmap \
-  16 blacklace[01][0-9].blacklace \
-  $PROJECT_FOLDER/data/assembled/aligned/megahit \
+  16 blacklace[01][016-9].blacklace \
+  $PROJECT_FOLDER/data/assembled/aligned \
   $PREFIX \
-  $PROJECT_FOLDER/data/assembled/megahit/$PREFIX/${PREFIX}.contigs.fa.gz \
+  $PROJECT_FOLDER/data/assembled/FINAL_COMBINED.assembly.fa \
   $FR \
   $RR \
   maxindel=100 \
   unpigz=t \
   touppercase=t \
-  path=$PROJECT_FOLDER/data/assembled/megahit/$PREFIX/ 
-  usemodulo=T 
+  path=$PROJECT_FOLDER/data/assembled \
+  -Xmx31g
 done
 
 # count overlapping features
-for BAM in $PROJECT_FOLDER/data/assembled/aligned/megahit/$P1*.bam; do
+for BAM in $PROJECT_FOLDER/data/assembled/aligned/*.bam; do
   $PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c coverage -p bam_count \
   blacklace[01][0-9].blacklace \
   $BAM \
@@ -65,7 +63,7 @@ for BAM in $PROJECT_FOLDER/data/assembled/aligned/megahit/$P1*.bam; do
 done
 
 # count bins
-Rscript $PROJECT_FOLDER/metagenomics_pipeline/scripts/cov_count.R "." "$P1.*\\.cov" "$PREFIX.countData"
+Rscript $PROJECT_FOLDER/metagenomics_pipeline/scripts/cov_count.R "." ".*\\.cov" "$PREFIX.countData"
 
 # Sub binning - convert cov to tab
 for F in $P1*.cov; do
@@ -73,5 +71,5 @@ for F in $P1*.cov; do
   awk -F"\t" '{sub("ID=","",$(NF-1));OUT=$1"_"$(NF-1)"_"$4"_"$5;print OUT,$(NF-1),$4,$5,$NF}' OFS="\t" $F > $O
 done 
 
-# parsing
+# get sub bin counts
 Rscript subbin_parser_v2.R reduced.txt *.tab $PREFIX.countData.sub_bins
