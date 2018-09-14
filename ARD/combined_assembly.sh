bbab2b671f44b51baeeac2f5148375cfb82c0e6d
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-09-14 16:26:39 +0100

    Update combined_assembly.sh

--- ARD/combined_assembly.sh
+++ ARD/combined_assembly.sh
@@ -1,3 +1,13 @@
+f=$(ls -m $PROJECT_FOLDER/data/corrected/B*_1.fq.gz|tr -d ' '|tr -d '\n')
+r=$(sed 's/1\.cleaned/2.cleaned/g' <<<$f)
+$PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
+# blacklace[01][06789].blacklace \
+10 blacklace01.blacklace \
+$PROJECT_FOLDER/data/assembled/megahit \
+BIGWOOD \
+-1 $f -2 $r --k-min=27 --k-step 10 --k-max 77
+
+
 # assemble spades MT assemblies + MT and MG corrected reads - everything assembly
 for F in $PROJECT_FOLDER/assembly/mt_mg_data/*.gz; do
   R=$(echo $R,$F)
