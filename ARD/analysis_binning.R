8ff6d58c6d6505108dfdab7690d5f1850235ec00
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-11-20 14:44:28 +0000

    Update analysis_binning.R

--- ARD/analysis_binning.R
+++ ARD/analysis_binning.R
@@ -8,31 +8,18 @@ library(tidyverse)
 # library(devtools)
 # install_github("eastmallingresearch/Metabarcoding_pipeline/scripts") (RUN ONCE)
 library(metafuncs)
-register(MulticoreParam(4)) # watch this as each process will take a copy the dds object - could use a lot of memory for sub_bins
+register(MulticoreParam(2)) # watch this as each process will take a copy the dds object - could use a lot of memory for sub_bins
 # MultiCore actually shares the memory between parent and worker processes. But, unfortunatly the memory gets copied on R scheduled garbage collection. Annoying
 #register(SnowParam(12))# No better - will just have to run on a high memory node (needs ~50G for Langdale sub_bin processing - with multiprocessing)
 
-SITES<-c("ATTINGHAM","BIGWOOD","CHESTNUTS","GT_MONK","LANGDALE","SPECULATION","WINDING")
-SITE <- SITES[1]
+SITE <- "COMB"
 #===============================================================================
 #       Load data
 #===============================================================================
-
-####  counts tables
-# get list of count tables
-#qq    <- lapply(list.files(".","*",full.names=T),function(x) {fread(x)})
-# get count file names and substitute to required format
-#names <- sub("(_ND.*_L)([0-9]*)(.*)","_\\2",list.files(".","*",full.names=F,recursive=F))
-# apply names to appropriate list columns (enables easy joining of all count tables)
-#lapply(seq(1:length(qq)),function(i) setnames(qq[[i]],"V2",names[i]))
-# merge count tables into single table
-#countData <- Reduce(function(...) {merge(..., all = TRUE)}, qq) # data table method (returns data table)
-#countData    <- qq %>% purr::reduce(full_join,by="V1") # plyr method (returns tibble - but not always...)
-
 ### count matrix (bins)
 countData <- fread(paste0(SITE,".countData"))
 setnames(countData,names(countData),sub("(_ND.*_L)([0-9]*)(.*)","_\\2",names(countData)))
-setnames(countData,"DOMAINS","PFAM_NAME")
+setnames(countData,"Domain","PFAM_NAME")
 
 # get number of reads mapped to domains       
 print(as.data.frame(colSums(countData[,-1])))
@@ -47,15 +34,8 @@ annotation <- fread("~/pipelines/common/resources/pfam/names.txt")
 setnames(annotation,"NAME","PFAM_NAME")
 pfam_go <- fread("~/pipelines/common/resources/mappings/pfam_go_map",header=F)
 
-# drop exact duplicates (subbins only) - not needed
-#countData$NAME <- sub("_clust0.*","",countData$V1)
-#countdata <- countData[!duplicated(countData[,-1]),-"NAME"]
-# countData <- countData[!duplicated(countData[,-1]),]
-# countData$BIN_ID <- paste0("BIN",seq(1,nrow(countData)))
-
 # map bins to pfam and go terms
 mapping_pfam <- annotation[countData[,1,with=F],on="PFAM_NAME"]
-
 mapping_pfam <- annotation[countData[,c(1,ncol(countData)),with=F],on="PFAM_NAME"]
 
 mapping_go   <- copy(mapping_pfam)
@@ -77,37 +57,28 @@ countData[, (colsToDelete) := NULL]
 
 # read in metadata
 colData   <- fread("colData")
-colData[,SampleID:=sub("_","_L",SampleID)] # but check names first - may not work for bigwood or speculation
-
        
 #===============================================================================
-#       Pool Data/subsample
+#       Subset DNA/RNA
 #===============================================================================
-# row_names column of countData object
+# ensure colData rows and countData columns have the same order
+colData <- colData[Sample==names(countData)[-1],]
+# remove low count and control samples
+myfilter <- colData$Type=="DNA"
+# apply filter
+colData2 <- copy(colData[which(myfilter==T),])
+countData2 <- copy(countData[,c(T,myfilter),with=F])
+
 row_names <- 1 
-
 # creates a dds object and also subsets and orders colData by colnames of countData
-dds <- DESeqDataSetFromMatrix(dt_to_df(countData,row_names), dt_to_df(colData)[names(countData)[-row_names],], ~1)
-
-# get number of samples per tree
-#sample_numbers <- table(sub("[A-Z]$","",dds$Sample))
-
-# collapse (mean) samples - could just use sum, then sizeFactors will correct
-dds <- collapseReplicates(dds,groupby=dds$Sample)
-
+dds <- DESeqDataSetFromMatrix(dt_to_df(countData2,row_names), dt_to_df(colData2)[names(countData2)[-row_names],], ~1)
 # get size factors
 sizeFactors(dds) <-sizeFactors(estimateSizeFactors(dds))
 
 #===============================================================================
 #       Differential analysis
 #===============================================================================
-
-# remove unhelpful counts (Attingham/Langdale)
-dds <- dds[,dds$Status!="Sandra"]
-
-# remove ambiguous coding
-colData(dds)$Status <- sub("_","",colData(dds)$Status)
-       
+      
 # filter out low counts (especially for subbins - there will be a lot with low counts)
 # dds <- dds[rowSums(counts(dds, normalize=T))>5,]
 
@@ -115,12 +86,11 @@ colData(dds)$Status <- sub("_","",colData(dds)$Status)
 alpha <- 0.1
 
 # the full model
-design <- ~Status # or
-design <- ~Block_pair+Status 
+design <- ~Pair+Status 
 
 # set any columns used in model to be factors (deseq should really do this internally...)
 dds$Status <- as.factor(dds$Status)
-dds$Block_pair <- as.factor(dds$Block_pair)
+dds$Pair <- as.factor(dds$Pair)
 
 # add full model to dds object
 design(dds) <- design
@@ -136,35 +106,6 @@ res_merge <- data.table(inner_join(data.table(PFAM_NAME=rownames(res),as.data.fr
 
 fwrite(res_merge,paste0(SITE,"_bins.txt"),sep="\t",quote=F)
 fwrite(res_merge[padj<=0.1,],paste0(SITE,"_bins_sig.txt"),sep="\t",quote=F)
-
-# Langdale/Attingham
-design <- ~Block_pair+Status # no results for ~Status model 
-LHS <- c("AOD","COD","Remission","AOD","COD","AOD")
-RHS <- c("Healthy","Healthy","Healthy","Remission","Remission","COD")
-
-res <- lapply(seq_along(LHS),function(i) {
-  results(dds,alpha=alpha,parallel=T,contrast=c("Status",LHS[i],RHS[i]))
-})
-
-invisible(lapply(res,summary))
-
-# bins
-res_merge <- lapply(res,function(res) {
-  data.table(inner_join(data.table(PFAM_NAME=rownames(res),as.data.frame(res)),annotation))
-})
-
-lapply(seq_along(res_merge),function(i) {
-  invisible(fwrite(res_merge[[i]],paste(SITE,LHS[i],RHS[i],"txt",sep="."),sep="\t",quote=F))
-})
-
-# sub bins
-res_merge <- lapply(res,function(res) {
-  data.table(inner_join(data.table(SUB_BIN_NAME=rownames(res),as.data.frame(res)),mapping_pfam))
-})
-
-lapply(seq_along(res_merge),function(i) {
-  invisible(fwrite(res_merge[[i]],paste(SITE,LHS[i],RHS[i],"status_sub_bin","txt",sep="."),sep="\t",quote=F))
-})
 
 #===============================================================================
 #       Functional analysis (sub bins) 
