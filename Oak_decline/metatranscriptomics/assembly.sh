81043f846efe0a01a996056bc697f0071ef01352
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-04-16 09:57:07 +0100

    Update assembly.sh

--- Oak_decline/metatranscriptomics/assembly.sh
+++ Oak_decline/metatranscriptomics/assembly.sh
@@ -1,22 +1,3 @@
-# Attingham - produces tmp files too large for tmp directory on nodes
-f=$(ls -m $PROJECT_FOLDER/data/corrected/A*_1.fq.gz|tr -d ' '|tr -d '\n')
-r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
-$PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
-24 blacklace11.blacklace \
-$PROJECT_FOLDER/data/assembled/megahit \
-ATTINGHAM \
--1 $f -2 $r --k-min=27 --k-step 10 --k-max 77 --tmp-dir /data/scratch/deakig/tmp/ATTINGHAM
-
-# Langdale - produces tmp files too large for tmp directory on nodes
-f=$(ls -m $PROJECT_FOLDER/data/corrected/L*_1.fq.gz|tr -d ' '|tr -d '\n')
-r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
-$PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
-24 blacklace11.blacklace \
-$PROJECT_FOLDER/data/assembled/megahit \
-LANGDALE \
--1 $f -2 $r --k-min=27 --k-step 10 --k-max 77 --tmp-dir /data/scratch/deakig/tmp/LANGDALE
-
-
 # combined assembly of metgenomics assembly + metatranscriptomic read
 
 # Attingham
@@ -34,3 +15,24 @@ pigz -d LANGDALE.contigs.fa.gz
 f=$(ls -m $PROJECT_FOLDER/data/corrected/A*_1.corrected.fq.gz|tr -d ' '|tr -d '\n')
 r=$(sed 's/1\.corrected/2.corrected/g' <<<$f)
 megahit -o OUTPUT -t 24 --kmin-1pass --out-prefix LANGDALE_COMB -1 $f -2 $r -r LANGDALE.contigs.fa --k-min=27 --k-step 10 --k-max 77 --tmp-dir /data/scratch/deakig/tmp/LANGDALE
+
+
+
+# single assemblies
+# Attingham - produces tmp files too large for tmp directory on nodes
+f=$(ls -m $PROJECT_FOLDER/data/corrected/A*_1.fq.gz|tr -d ' '|tr -d '\n')
+r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
+$PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
+24 blacklace11.blacklace \
+$PROJECT_FOLDER/data/assembled/megahit \
+ATTINGHAM \
+-1 $f -2 $r --k-min=27 --k-step 10 --k-max 77 --tmp-dir /data/scratch/deakig/tmp/ATTINGHAM
+
+# Langdale - produces tmp files too large for tmp directory on nodes
+f=$(ls -m $PROJECT_FOLDER/data/corrected/L*_1.fq.gz|tr -d ' '|tr -d '\n')
+r=$(sed 's/1\.fq\.gz/2.fq.gz/g' <<<$f)
+$PROJECT_FOLDER/metagenomics_pipeline/scripts/PIPELINE.sh -c assemble -p megahit2 \
+24 blacklace11.blacklace \
+$PROJECT_FOLDER/data/assembled/megahit \
+LANGDALE \
+-1 $f -2 $r --k-min=27 --k-step 10 --k-max 77 --tmp-dir /data/scratch/deakig/tmp/LANGDALE
