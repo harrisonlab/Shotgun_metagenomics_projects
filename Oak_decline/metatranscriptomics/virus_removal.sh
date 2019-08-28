d9f476e73ce9b960814f5d0d5fe22bf4aba587f3
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-08-28 16:53:52 +0100

    Update virus_removal.sh

--- Oak_decline/metatranscriptomics/virus_removal.sh
+++ Oak_decline/metatranscriptomics/virus_removal.sh
@@ -6,7 +6,14 @@ gunzip viral* -c |cat - > virus.fa
 # create usearch index of viruses
 usearch -makeudb_usearch virus.fa -output idx.udb
 
+# single line protein file
+awk '/^>/ {printf("\n%s\n",$0);next; } { printf("%s",$0);}  END {printf("\n");}' < LANGDALE.pep| tail -n +2 > LANGDALE.sl.pep
+
 # split hhmer peptides into chunks
-awk
-L=$(( $(wc -l <LANDALE.pep) / 10 )
-split -l $L virus.sl.fa.
+L=$(( $(wc -l <LANDGALE.sl.pep) / 10 )
+split -l $L out.pep.
+
+for f in out.pep*; do
+  usearch -usearch_global $f -db idx.udb -id 0.25 -blast6out ${f}.out; 
+done
+
