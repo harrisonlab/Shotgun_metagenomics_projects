860cfce90b68a68fab9f7d694053317dcfe60c54
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-12-03 13:33:28 +0000

    Update analysis.sh

--- Oak_decline/anvio/analysis.sh
+++ Oak_decline/anvio/analysis.sh
@@ -5,10 +5,16 @@ sed 's/.*_c/c/'|
 awk -F":" '{count++;if ($3>$2){d="f"}else{d="r";t=$2;$2=$3;$3=t};p=1;$3=$3+1;if(($3-$2)%3==0){p=0};print count,$1,$2,$3,d,p,"program","v1.0"}' OFS="\t" > t2.out
 
 # generate anvio database - 2 methods depending whether there are gene calls already available
-anvi-gen-contigs-database -f test.fa -o test.db -n "test database"  --external-gene-calls t2.out
+
+## add header for first method
+sed -i '1 i\gene_callers_id\tcontig\tstart\tstop\tdirection\tpartial\t\source\tversion' test.out
+anvi-gen-contigs-database -f test.fa -o test.db -n "test database"  --external-gene-calls test.out
+
+## no header for second method
 anvi-gen-contigs-database -f test.fa -o test.db -n "test database" --skip-gene-calling 
-
-# import ORFs into sqlite database (use  Control-v <TAB> for separator)
-sqlite3 -separator ' ' test.db  ".import t2.out genes_in_contigs"
-
-# extract and translate 
+## import ORFs into sqlite database (use  Control-v <TAB> for separator)
+sqlite3 -separator ' ' test.db  ".import test.out genes_in_contigs"
+## test sql import has worked
+sqlite3 test.db "select * from genes_in_contigs limit 10"
+## extract and translate 
+anvi-get-sequences-for-gene-calls -c test.db -o test.prot.fa
