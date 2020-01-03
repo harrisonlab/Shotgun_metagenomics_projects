bdca625375aec30445c3110b6551b9411ce52535
Greg Deakin <GregDeakin@users.noreply.github.com>
2020-01-03 14:32:29 +0000

    Update analysis.sh

--- Oak_decline/anvio/analysis.sh
+++ Oak_decline/anvio/analysis.sh
@@ -1,7 +1,7 @@
 # use ORFfinder to find - bacterial ORFS
 ORFfinder -in test.fa -g 11 -s 1 -ml 180 -n true -outfmt 3 -out temp.out
 grep ">" temp.out |
-sed 's/.*_c/c/'|
+sed 's/.*_k/k/'|
 awk -F":" '{count++;if ($3>$2){d="f"}else{d="r";t=$2;$2=$3;$3=t};p=1;$3=$3+1;if(($3-$2)%3==0){p=0};print count,$1,$2,$3,d,p,"program","v1.0"}' OFS="\t" > t2.out
 
 # generate anvio database - 2 methods depending whether there are gene calls already available
