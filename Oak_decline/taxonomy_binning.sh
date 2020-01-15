a3d3d2ed88afefb4ac81572b6c4c75194d55ec9f
Greg Deakin <GregDeakin@users.noreply.github.com>
2020-01-15 11:05:32 +0000

    Update taxonomy_binning.sh

--- Oak_decline/taxonomy_binning.sh
+++ Oak_decline/taxonomy_binning.sh
@@ -34,3 +34,30 @@ runMetaBat.sh  --unbinned -m 1500 -x 0 --minCVSum 0.5 \
 jgi_summarize_bam_contig_depths --outputDepth GTMONKdepth.txt ../sorted/G*
 metabat2  --unbinned -m 1500 -x 0 --minCVSum 0.5 \
 -i ~/projects/Oak_decline/metagenomics/data/assembled/megahit/GTMONK/GTMONK.contigs.fa -a GTMONKdepth.txt -o bins_dir/bin 
+
+
+# Taxonomy assignment with kaiju
+kaiju-makedb -s nr_euk 
+# kaiju-makedb -s nr_euk --index-only -t 12 --no-download
+kaiju -t nodes.dmp -f kaiju_db.fmi -i fortaxa.fa -o kaiju.out -z 10 -v
+
+# bad method as has to read in index each time - slow
+for f in bin.1.fa; do
+  kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i $f -o $f.kaiju.out -z 10 -v
+done
+
+# need to concatenate the bins, with the bin name in the fasta header
+for f in bin*.fa; do
+ #echo $f
+ sed -i -e "s/k/$f.k/" $f
+done
+
+cat bin*.fa >ATTINGHAM.bins.fa
+cat bin*.fa >GTMONK.bins.fa
+cat bin*.fa >LANGDALE.bins.fa
+cat bin*.fa >WINDING.bins.fa
+
+kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i ATTINGHAM.bins.fa -o ATTINGHAM.kaiju.out -z 20 -v
+kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i GTMONK.bins.fa -o GTMONK.kaiju.out -z 20 -v
+kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i LANGDALE.bins.fa -o LANGDALE.kaiju.out -z 20 -v
+kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i WINDING.bins.fa -o WINDING.kaiju.out -z 20 -v
