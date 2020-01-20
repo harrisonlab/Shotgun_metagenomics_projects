da6dfb3dc4d3539244ecb6efcfefdcb74e87c1a9
Greg Deakin <GregDeakin@users.noreply.github.com>
2020-01-20 09:56:55 +0000

    Update transcriptome.sh

--- Oak_decline/taxonomy/transcriptome.sh
+++ Oak_decline/taxonomy/transcriptome.sh
@@ -1,1 +1,12 @@
+# Make salmon index from each of the combined bin files
+salmon index -t ~/projects/Oak_decline/metagenomics/data/taxonomy/ATTINGHAM_BINS/ATTINGHAM.bins.fa \
+-i ~/projects/Oak_decline/metagenomics/data/taxonomy/ATTINGHAM_BINS/salmon
 
+salmon index -t ~/projects/Oak_decline/metagenomics/data/taxonomy/GTMONK_BINS/GTMONK.bins.fa \
+-i ~/projects/Oak_decline/metagenomics/data/taxonomy/GTMONK_BINS/salmon
+
+salmon index -t ~/projects/Oak_decline/metagenomics/data/taxonomy/LANGDALE_BINS/LANGDALE.bins.fa \
+-i ~/projects/Oak_decline/metagenomics/data/taxonomy/LANGDALE_BINS/salmon
+
+salmon index -t ~/projects/Oak_decline/metagenomics/data/taxonomy/WINDING_BINS/WINDING.bins.fa \
+-i ~/projects/Oak_decline/metagenomics/data/taxonomy/WINDING_BINS/salmon
