ff94327714836f87b86f720990e1428260395d81
Greg Deakin <GregDeakin@users.noreply.github.com>
2020-01-28 12:32:32 +0000

    Update taxonomy_binning.sh

--- Oak_decline/taxonomy/taxonomy_binning.sh
+++ Oak_decline/taxonomy/taxonomy_binning.sh
@@ -1,91 +1,104 @@
 # binning using metabat
 # pipeline needs sorted bam files
 
+PROJECT_FOLDER=~/projects/Oak_decline/metatranscriptomics
+PREFIX=SPECULATION # and etc.
+P1=${PREFIX:0:1}
+
 # sort bam files
 for f in *.bam; do
  PREFIX=$(echo $f|sed -e 's/\..*//')
  sbatch -p medium --mem-per-cpu 4500M -c 8 \
- ~/pipelines/metagenomics/scripts/slurm/sub_bam_sort.sh \
- 8 /data/data2/scratch2/deakig/Oak/sorted $PREFIX /data/data2/scratch2/deakig/Oak/to_sort/$f
+ $PROJECT_FOLDER/metagenomics_pipeline/scripts/slurm/sub_bam_sort.sh \
+ 8 $PROJECT_FOLDER/data/sorted $PREFIX /data/data2/scratch2/deakig/Oak/to_sort/$f
 done
 
 # get list of bam files for each assembly
-A=$(for f in ../sorted/A*; do echo $f; done|tr  '\n' ' ')
-G=$(for f in ../sorted/G*; do echo $f; done|tr  '\n' ' ')
-L=$(for f in ../sorted/L*; do echo $f; done|tr  '\n' ' ')
-W=$(for f in ../sorted/W*; do echo $f; done|tr  '\n' ' ')
-
-B=$(for f in ../sorted/B*; do echo $f; done|tr  '\n' ' ')
-C=$(for f in ../sorted/C*; do echo $f; done|tr  '\n' ' ')
-S=$(for f in ../sorted/S*; do echo $f; done|tr  '\n' ' ')
+BAMS=$(for f in $PROJECT_FOLDER/data/sorted/$P1*; do echo $f; done|tr  '\n' ' ')
+#A=$(for f in $PROJECT_FOLDER/data/sorted/A*; do echo $f; done|tr  '\n' ' ')
+# G=$(for f in $PROJECT_FOLDER/data/sorted/G*; do echo $f; done|tr  '\n' ' ')
+# L=$(for f in $PROJECT_FOLDER/data/sorted/L*; do echo $f; done|tr  '\n' ' ')
+# W=$(for f in $PROJECT_FOLDER/data/sorted/W*; do echo $f; done|tr  '\n' ' ')
 
 # run metabat
+sbatch --mem=40000 -p medium $PROJECT_FOLDER/metagenomics_pipeline/scripts/slurm/sub_metabat.sh \
+$PROJECT_FOLDER/data/assembled/megahit/$PREFIX/$PREFIX.contigs.fa.gz \
+$PROJECT_FOLDER/data/taxonomy_binning/${PREFIX}_BINS $BAMS
+
 # runMetaBat.sh -i assembly.fa.gz --unbinned -o assembly_name -m 1500 -x 0 --minCVSum 0.5 bam_files 
 
-runMetaBat.sh  --unbinned -m 1500 -x 0 --minCVSum 0.5 \
-~/projects/Oak_decline/metagenomics/data/assembled/megahit/ATTINGHAM/ATTINGHAM.contigs.fa $A &
+# runMetaBat.sh  --unbinned -m 1500 -x 0 --minCVSum 0.5 \
+# ~/projects/Oak_decline/metagenomics/data/assembled/megahit/ATTINGHAM/ATTINGHAM.contigs.fa $A &
 
-runMetaBat.sh  --unbinned -m 1500 -x 0 --minCVSum 0.5 \
-~/projects/Oak_decline/metagenomics/data/assembled/megahit/GTMONK/GTMONK.contigs.fa $G &
+# runMetaBat.sh  --unbinned -m 1500 -x 0 --minCVSum 0.5 \
+# ~/projects/Oak_decline/metagenomics/data/assembled/megahit/GTMONK/GTMONK.contigs.fa $G &
 
-runMetaBat.sh  --unbinned -m 1500 -x 0 --minCVSum 0.5 \
-~/projects/Oak_decline/metagenomics/data/assembled/megahit/LANGDALE/LANGDALE.contigs.fa $L &
+# runMetaBat.sh  --unbinned -m 1500 -x 0 --minCVSum 0.5 \
+# ~/projects/Oak_decline/metagenomics/data/assembled/megahit/LANGDALE/LANGDALE.contigs.fa $L &
 
-runMetaBat.sh  --unbinned -m 1500 -x 0 --minCVSum 0.5 \
-~/projects/Oak_decline/metagenomics/data/assembled/megahit/WINDING/WINDING.contigs.fa $W &
+# runMetaBat.sh  --unbinned -m 1500 -x 0 --minCVSum 0.5 \
+# ~/projects/Oak_decline/metagenomics/data/assembled/megahit/WINDING/WINDING.contigs.fa $W &
 
-jgi_summarize_bam_contig_depths --outputDepth GTMONKdepth.txt ../sorted/G*
-metabat2  --unbinned -m 1500 -x 0 --minCVSum 0.5 \
--i ~/projects/Oak_decline/metagenomics/data/assembled/megahit/GTMONK/GTMONK.contigs.fa -a GTMONKdepth.txt -o bins_dir/bin 
+#jgi_summarize_bam_contig_depths --outputDepth GTMONKdepth.txt ../sorted/G*
+#metabat2  --unbinned -m 1500 -x 0 --minCVSum 0.5 \
+# -i ~/projects/Oak_decline/metagenomics/data/assembled/megahit/GTMONK/GTMONK.contigs.fa -a GTMONKdepth.txt -o bins_dir/bin 
 
 
 # Taxonomy assignment with kaiju
-kaiju-makedb -s nr_euk 
+# kaiju-makedb -s nr_euk 
 # kaiju-makedb -s nr_euk --index-only -t 12 --no-download
-kaiju -t nodes.dmp -f kaiju_db.fmi -i fortaxa.fa -o kaiju.out -z 10 -v
+# kaiju -t nodes.dmp -f kaiju_db.fmi -i fortaxa.fa -o kaiju.out -z 10 -v
 
 # bad method as has to read in index each time - slow
-for f in bin.1.fa; do
-  kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i $f -o $f.kaiju.out -z 10 -v
+#for f in bin.1.fa; do
+#  kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i $f -o $f.kaiju.out -z 10 -v
+# done
+
+# concatenate bins capturing bin name
+for f in $PROJECT_FOLDER/data/taxonomy_binning/$PREFIX_BINS/*.fa; do
+ sed -e "s/>k/>${f}.k/" $f >>$PREFIX.bins.fa
 done
 
-### THIS IS WRONG - need bin name
-cat bin*.fa >ATTINGHAM.bins.fa
-cat bin*.fa >GTMONK.bins.fa
-cat bin*.fa >LANGDALE.bins.fa
-cat bin*.fa >WINDING.bins.fa
-###
+# run kaiju for taxonomic assignment of bins
+sbatch --mem=120000 -p medium -c 20 $PROJECT_FOLDER/metagenomics_pipeline/scripts/slurm/sub_kaiju.sh \
+$PROJECT_FOLDER/data/kaiju/nodes.dmp \
+$PROJECT_FOLDER/data/kaiju/nr_euk/kaiju_db_nr_euk.fmi \
+$PROJECT_FOLDER/data/taxonomy_binning/${PREFIX}_BINS/${PREFIX}.bins.fa \
+${PREFIX}.kaiju.out \
+$PROJECT_FOLDER/data/taxonomy/$PREFIX
 
+#kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i ATTINGHAM.bins.fa -o ATTINGHAM.kaiju.out -z 20 -v
+#kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i ATTINGHAM.bins.fa -o ATTINGHAM.kaiju.out -z 20 -v
+#kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i GTMONK.bins.fa -o GTMONK.kaiju.out -z 20 -v
+#kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i LANGDALE.bins.fa -o LANGDALE.kaiju.out -z 20 -v
+#kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i WINDING.bins.fa -o WINDING.kaiju.out -z 20 -v
 
-kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i ATTINGHAM.bins.fa -o ATTINGHAM.kaiju.out -z 20 -v
-kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i GTMONK.bins.fa -o GTMONK.kaiju.out -z 20 -v
-kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i LANGDALE.bins.fa -o LANGDALE.kaiju.out -z 20 -v
-kaiju -t ../../../kaiju/nodes.dmp -f ../../../kaiju/nr_euk/kaiju_db_nr_euk.fmi -i WINDING.bins.fa -o WINDING.kaiju.out -z 20 -v
-
-# Total taxonomy counts
-f=ATTINGHAM
-kaiju2table -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r phylum -o $f.phylum.tsv $f.kaiju.out &
-kaiju2table -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r class -o $f.class.tsv $f.kaiju.out &
-kaiju2table -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r order -o $f.order.tsv $f.kaiju.out &
-kaiju2table -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r family -o $f.family.tsv $f.kaiju.out &
-kaiju2table -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r genus -o $f.genus.tsv $f.kaiju.out &
-kaiju2table -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r species -l superkingdom,phylum,class,order,family,genus,species -o $f.species.tsv $f.kaiju.out &
+# Total taxonomy counts - waste of time, can all be done with kaiju-addTaxonNames and a very simple R script
+#f=ATTINGHAM
+#kaiju2table -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r phylum -o $f.phylum.tsv $f.kaiju.out &
+#kaiju2table -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r class -o $f.class.tsv $f.kaiju.out &
+#kaiju2table -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r order -o $f.order.tsv $f.kaiju.out &
+#kaiju2table -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r family -o $f.family.tsv $f.kaiju.out &
+#kaiju2table -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r genus -o $f.genus.tsv $f.kaiju.out &
+#kaiju2table -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r species -l superkingdom,phylum,class,order,family,genus,species -o $f.species.tsv $f.kaiju.out &
 
 # This is better - add taxon names to the output
-kaiju-addTaxonNames -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r superkingdom,phylum,class,order,family,genus,species -i ATTINGHAM.kaiju.out -o ATTINGHAM.names.out &
-kaiju-addTaxonNames -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r superkingdom,phylum,class,order,family,genus,species -i GTMONK.kaiju.out -o GTMONK.names.out &
-kaiju-addTaxonNames -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r superkingdom,phylum,class,order,family,genus,species -i LANGDALE.kaiju.out -o LANGDALE.names.out &
-kaiju-addTaxonNames -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r superkingdom,phylum,class,order,family,genus,species -i WINDING.kaiju.out -o WINDING.names.out &
+kaiju-addTaxonNames -t $PROJECT_FOLDER/data/kaiju/nodes.dmp -n $PROJECT_FOLDER/data/kaiju/names.dmp -r superkingdom,phylum,class,order,family,genus,species -i ${PREFIX}.kaiju.out -o ${PREFIX}.names.out &
+
+#kaiju-addTaxonNames -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r superkingdom,phylum,class,order,family,genus,species -i ATTINGHAM.kaiju.out -o ATTINGHAM.names.out &
+#kaiju-addTaxonNames -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r superkingdom,phylum,class,order,family,genus,species -i GTMONK.kaiju.out -o GTMONK.names.out &
+#kaiju-addTaxonNames -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r superkingdom,phylum,class,order,family,genus,species -i LANGDALE.kaiju.out -o LANGDALE.names.out &
+#kaiju-addTaxonNames -t ../../../kaiju/nodes.dmp -n ../../../kaiju/names.dmp -r superkingdom,phylum,class,order,family,genus,species -i WINDING.kaiju.out -o WINDING.names.out &
 
 # Get protein names from nr database (sqlite is fairly quick for this sort of query)
 zgrep ">.*?\[" -oP nr.gz |sed 's/..$//'|sed 's/>//'|sed 's/MULTIGENE: //'|sed 's/ /|/' >nr.names
 sqlite3 nr.db "CREATE TABLE nr(acc TEXT PRIMARY KEY, desc TEXT)"
 sqlite3 -separator "|" nr.db ".import nr.names nr" 2>/dev/null
 
-#echo "SELECT * FROM nr WHERE " >script.sql
-#awk -F"\t" '{print $6}' OFS="," LANGDALE.kaiju.out|sed 's/.$//'|awk -F"," '{ for(i = 1; i <= NF; i++) { print "acc=\x27"$i"\x27 OR"; } }'|sed '$s/OR//' >>script.sql
-#sqlite3 /data/data2/scratch2/deakig/kaiju/nr_euk/nr.db <script.sql > LANGDALE.prots.out
-# nsqlite3 has a limit on the complexity of queries - 10000 OR statements
+# echo "SELECT * FROM nr WHERE " >script.sql
+# awk -F"\t" '{print $6}' OFS="," LANGDALE.kaiju.out|sed 's/.$//'|awk -F"," '{ for(i = 1; i <= NF; i++) { print "acc=\x27"$i"\x27 OR"; } }'|sed '$s/OR//' >>script.sql
+# sqlite3 /data/data2/scratch2/deakig/kaiju/nr_euk/nr.db <script.sql > LANGDALE.prots.out
+# sqlite3 has a limit on the complexity of queries - 10000 OR statements
 awk -F"\t" '{print $6}' OFS="," ATTINGHAM.kaiju.out|sed 's/.$//'|awk -F"," '{ for(i = 1; i <= NF; i++) { print "acc=\x27"$i"\x27 OR"; } }'|sed '$s/OR//'|split -l 9999
 for f in x*; do 
  sed -i -e '$s/OR//' $f
@@ -120,9 +133,6 @@ echo awk '{
 ./script.sh WINDING_BINS/WINDING.bins.fa > WINDING_BINS/WINDING.gff &
 
 ## count overlapping features
-PROJECT_FOLDER=~/projects/Oak_decline/metagenomics
-PREFIX=ATTINGHAM # and etc.
-P1=${PREFIX:0:1}
 
 for BAM in $PROJECT_FOLDER/data/aligned/$P1*; do
   sbatch --mem 40000 $PROJECT_FOLDER/metagenomics_pipeline/scripts/slurm/sub_bam_count.sh \
